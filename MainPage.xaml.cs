1fcba5dc15cdd62cd7c4a4908c79da5831546dc1
Seyyahtar <aazedat@hotmail.com>
2025-11-12 23:11:26 +0300

    Add case entry page and center settings shortcut

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,15 @@
-﻿namespace Nihai
+namespace Nihai
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private async void OnCaseCardTapped(object sender, TappedEventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            await Shell.Current.GoToAsync(nameof(CasePage));
         }
     }
 }
